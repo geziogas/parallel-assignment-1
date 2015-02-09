49a0bdcc0c52a3487f7feba4240b16c714510e5e
geziogas <gs.ziogas@gmail.com>
2015-02-09 22:34:53 +0200

    Simple start...
    Main part to create A and B random matrices, and C empty matrix with
    given size n x n.

--- assignment1.c
+++ assignment1.c
@@ -4,10 +4,73 @@
        Authors: Georgios Ziogas, Charalampos Kominos, Stavros Mavrakis
 
 *********************************************************************/
-#include <mpi.h>
+// #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
 
+	// Variables
+	int n,p,row,col;
+
+	// Check for wrong number of arguments
+	printf("\n");
+	if (argc!=3){
+    	printf("Wrong number of arguments.\nneeded 2 (Matrices Size, Processors)\n");    
+    	return 0;
+  	}
+
+	// Converts string arguments to int
+	n = atoi(argv[1]);
+  	p = atoi(argv[2]);
+  	printf("Size of matrices: %d\n",n);
+  	printf("number of processors: %d\n",p);
+
+  	// Set matrices according to given size
+  	double *A,*B,*C;
+  	A=(double *)calloc(n*n,sizeof(double ));
+  	B=(double *)calloc(n*n,sizeof(double ));
+  	C=(double *)calloc(n*n,sizeof(double )); //Result-matrix
+
+  	srand(time(NULL));
+  	for(row=0;row<n;row++){
+		for(col=0;col<n;col++)
+		{
+			A[row*n+col]=rand()/(double)RAND_MAX;
+			B[row*n+col]=rand()/(double)RAND_MAX;
+		}
+	}
+
+	// Test code, to show the 2 random generated matrices
+	printf("\nMatrix A:\n");
+	for(row=0;row<n;row++){
+		for(col=0;col<n;col++)
+		{
+			printf("%f ",A[row*n+col]);						
+		}
+		printf("\n");
+	}
+
+	printf("\nMatrix B:\n");
+	for(row=0;row<n;row++){
+		for(col=0;col<n;col++)
+		{
+			printf("%f ",B[row*n+col]);
+		}
+		printf("\n");
+	}
+
+	printf("\nMatrix C:\n");
+	for(row=0;row<n;row++){
+		for(col=0;col<n;col++)
+		{
+			printf("%f ",C[row*n+col]);
+		}
+		printf("\n");
+	}
+
+	free(A),free(B),free(C);
+  	return 0;
 }
