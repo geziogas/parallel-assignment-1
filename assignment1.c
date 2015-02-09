ef08183b3290c9b7d75108b17fedcf2b3a263736
geziogas <gs.ziogas@gmail.com>
2015-02-09 20:42:19 +0200

    Start of assignment 1

---
+++ assignment1.c
@@ -0,0 +1,13 @@
+/*********************************************************************
+       Assignment 1 / Programming of Parallel Computers
+
+       Authors: Georgios Ziogas, Charalampos Kominos, Stavros Mavrakis
+
+*********************************************************************/
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+}
