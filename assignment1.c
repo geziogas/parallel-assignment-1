f153edc82b32b479bd1e2e76abe7426948757d43
geziogas <gs.ziogas@gmail.com>
2015-02-10 21:26:14 +0200

    grid create

--- assignment1.c
+++ assignment1.c
@@ -4,16 +4,16 @@
        Authors: Georgios Ziogas, Charalampos Kominos, Stavros Mavrakis
 
 *********************************************************************/
-// #include <mpi.h>
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 int main(int argc, char *argv[]) 
 {
-//test
+
 	// Variables
-	int n,p,row,col;
+	int n,p,row,col,size,myid;
 
 	// Check for wrong number of arguments
 	printf("\n");
@@ -76,7 +76,42 @@ int main(int argc, char *argv[])
 		printf("\n");
 	}
 
+	MPI_Comm proc_grid, proc_row, proc_col;
+  	int coords[2],pos[2],reorder=1,ndim=2,dims[2]={0,0},periods[2]={0,0};
+  	int subrankRow, subrankCol;
+  	MPI_Status stat;
+
+	MPI_Init(&argc, &argv);               /* Initialize MPI               */
+  	MPI_Comm_size(MPI_COMM_WORLD, &size); /* Get the number of processors */
+  	// MPI_Comm_rank(MPI_COMM_WORLD, &rank); /* Get my number    			  */
+	
+  	/* Create a virtual 2D-grid topology */
+	MPI_Dims_create(size,ndim,dims);
+	MPI_Cart_create(MPI_COMM_WORLD,ndim,dims,periods,reorder,&proc_grid);
+	MPI_Comm_rank(proc_grid,&myid);    /* Note: use proc_grid */
+
+	/* Create a communicator for each row */
+	MPI_Cart_coords(proc_grid,myid,ndim,coords); 
+	MPI_Comm_split(proc_grid,coords[0],coords[1],&proc_row);
+	MPI_Comm_rank(proc_row,&subrankRow); 
+
+  	/* Create a communicator for each column */
+	MPI_Cart_coords(proc_grid,myid,ndim,coords); 
+	MPI_Comm_split(proc_grid,coords[1],coords[0],&proc_col);
+	MPI_Comm_rank(proc_col,&subrankCol); 
+
+	if(myid==0) {
+		printf("I am the root. Number of processes: %d\n",size);
+	}
+
+	printf("Welcome. I am process No %d / coords: %d.%d\n",myid,subrankRow,subrankCol);
+	printf("\n");
+	
 	// Free memory after execution
 	free(A),free(B),free(C);
+	
+	// Shut down and clean up MPI   
+	MPI_Finalize(); 
+
   	return 0;
 }
