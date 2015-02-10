814b00f26b367fca792b8632ed457df77a1325bf
smavrakis <smavrakis3@hotmail.com>
2015-02-10 20:18:38 +0100

    Split processes

--- assignment1.c
+++ assignment1.c
@@ -9,26 +9,29 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-//test
+
 	// Variables
-	int n,p,row,col;
+	int n,row,col,nproc,my_id,row_rank,column_rank,row_size,column_size;
+	MPI_Comm proc_grid,proc_row,proc_column;
+	int coords[2],pos[2],reorder=1,ndim=2,dims[2]={0,0},periods[2]={0,0};
+
 
 	// Check for wrong number of arguments
 	printf("\n");
 	if (argc!=3){
-    	printf("Wrong number of arguments.\nneeded 2 (Matrices Size, Processors)\n");    
+    	printf("Wrong number of arguments.\nneeded 2 (Matrices Size, Processors)\n");
     	return 0;
   	}
 
 	// Converts string arguments to int
 	n = atoi(argv[1]);
-  	p = atoi(argv[2]);
+	nproc = atoi(argv[2]);
   	printf("Size of matrices: %d\n",n);
-  	printf("number of processors: %d\n",p);
+  	printf("number of processors: %d\n",nproc);
 
-  	// Set matrices according to given size
+  	// Create matrices according to given size
   	double *A,*B,*C;
   	A=(double *)calloc(n*n,sizeof(double ));
   	B=(double *)calloc(n*n,sizeof(double ));
@@ -42,18 +45,18 @@ int main(int argc, char *argv[])
 
   	for(row=0;row<n;row++){
 		for(col=0;col<n;col++)
-		{			
+		{
 			A[row*n+col]=rand()/divisor;
 			B[row*n+col]=rand()/divisor;
 		}
 	}
 
 	// Test code, to show the 2 random generated matrices
-	printf("\nMatrix A:\n");
+	/*printf("\nMatrix A:\n");
 	for(row=0;row<n;row++){
 		for(col=0;col<n;col++)
 		{
-			printf("%f ",A[row*n+col]);						
+			printf("%f ",A[row*n+col]);
 		}
 		printf("\n");
 	}
@@ -74,9 +77,34 @@ int main(int argc, char *argv[])
 			printf("%f ",C[row*n+col]);
 		}
 		printf("\n");
-	}
+	}*/
 
-	// Free memory after execution
+  	// Initialize MPI
+  	MPI_Init(&argc,&argv);
+  	MPI_Comm_size(MPI_COMM_WORLD,&nproc);
+
+  	// Create a virtual 2D-grid topology
+  	MPI_Dims_create(nproc,ndim,dims);
+  	MPI_Cart_create(MPI_COMM_WORLD,ndim,dims,periods,reorder,&proc_grid);
+  	MPI_Comm_rank(proc_grid,&my_id);
+  	MPI_Cart_coords(proc_grid,my_id,ndim,coords);
+
+  	// Create a communicator for each row
+  	MPI_Comm_split(proc_grid,coords[0],coords[1],&proc_row);
+  	MPI_Comm_rank(proc_row,&row_rank);
+  	MPI_Comm_size(proc_row,&row_size); // De kserw akoma pou to xrisimopoioun auto, den koitaksa parakatw ton kwdika
+
+  	// Create a communicator for each column
+  	MPI_Comm_split(proc_grid,coords[1],coords[0],&proc_column);
+  	MPI_Comm_rank(proc_column,&column_rank);
+  	MPI_Comm_size(proc_column,&column_size); // De kserw akoma pou to xrisimopoioun auto, den koitaksa parakatw ton kwdika
+
+  	// Free memory after execution
+  	MPI_Comm_free(&proc_row);
+  	MPI_Comm_free(&proc_column);
+  	MPI_Comm_free(&proc_grid);
+  	MPI_Finalize();
+
 	free(A),free(B),free(C);
   	return 0;
 }
