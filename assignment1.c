19b8e46a0862a033832adaa9645a22c0508dd2f4
smavrakis <smavrakis3@hotmail.com>
2015-02-12 17:31:36 +0100

    Transmit blocks to processes

--- assignment1.c
+++ assignment1.c
@@ -16,9 +16,15 @@ int main(int argc, char *argv[])
 	// Variables
 	int root = 0; //The root process
 	int n,row,col,nproc,my_id,row_rank,column_rank,row_size,column_size;
-	int blocklen,blocks;
-	MPI_Comm proc_grid,proc_row,proc_column;
 	int coords[2],pos[2],reorder=1,ndim=2,dims[2]={0,0},periods[2]={0,0};
+	int x,count,blocklength,stride,limit,temp_rank;
+	double *blockA,*blockB,*blockC;
+	double *A,*B,*C;
+	clock_t start_time,end_time;
+	MPI_Comm proc_grid,proc_row,proc_column;
+	MPI_Datatype newtype;
+	MPI_Request r1,r2;
+	MPI_Status status;
 
 
 	// Initialize MPI
@@ -34,12 +40,12 @@ int main(int argc, char *argv[])
   	// Create a communicator for each row
   	MPI_Comm_split(proc_grid,coords[0],coords[1],&proc_row);
   	MPI_Comm_rank(proc_row,&row_rank);
-  	MPI_Comm_size(proc_row,&row_size); // De kserw akoma pou to xrisimopoioun auto, den koitaksa parakatw ton kwdika
+  	MPI_Comm_size(proc_row,&row_size);
 
   	// Create a communicator for each column
   	MPI_Comm_split(proc_grid,coords[1],coords[0],&proc_column);
   	MPI_Comm_rank(proc_column,&column_rank);
-  	MPI_Comm_size(proc_column,&column_size); // De kserw akoma pou to xrisimopoioun auto, den koitaksa parakatw ton kwdika
+  	MPI_Comm_size(proc_column,&column_size);
 
 
 	// Check for wrong number of arguments
@@ -55,27 +61,26 @@ int main(int argc, char *argv[])
   	}
 
 	// Converts string arguments to int
-	n = atoi(argv[1]);	
+	n = atoi(argv[1]);
 
 	if (my_id==root)
 	{
 		printf("I am the root process.\n");
   		printf("Size of matrices: %d\n",n);
-  		printf("number of processors: %d\n",nproc);
+  		printf("number of processes: %d\n",nproc);
 	}
-
-  	// Create matrices according to given size
-  	double *A,*B,*C;
-  	A=(double *)calloc(n*n,sizeof(double ));
-  	B=(double *)calloc(n*n,sizeof(double ));
-  	C=(double *)calloc(n*n,sizeof(double )); //Result-matrix
 
 
   	// Set and show the matrices by the root process
-  	if (my_id==root) 
+  	if (my_id==root)
   	{
-	  	srand(time(NULL)); //Used to change the seed according to time.
 
+  		// Create matrices according to given size
+  		A=(double *)calloc(n*n,sizeof(double ));
+  		B=(double *)calloc(n*n,sizeof(double ));
+  		C=(double *)calloc(n*n,sizeof(double )); //Result-matrix
+
+	  	srand(time(NULL)); //Used to change the seed according to time.
 
 	  	// Set the random limit number
 	  	double scaleLimit = 100.0;
@@ -120,40 +125,51 @@ int main(int argc, char *argv[])
 		}
 
 		printf("\n");
-	}  	
-	
-  	
-	printf("Welcome. I am process No %d / coords: %d.%d\n",my_id,row_rank,column_rank);
-	
-	//first task ->broadcast blocks
-	//fox algorithm
-	//Allgather???
-	//print apo ton rank 0??
-	//na grapsoume kai serial code???
-	/************************************************************
-	Part where we divide the matrices into blocks
-	We assume for simplicity that the partitioning is performed
-	like :  partition-length is the square root of nproc.
-	Nx = Ny = square_root(nproc)
-	*************************************************************/
-	
-	blocks = sqrt(nproc); // One dimension number of blocks
-	blocklen = n/blocks;	  // length of each block
-	
-	if(my_id==root) {
-		printf("Length of block to send: %d\n", blocklen);
-	} 
-	 // MPI_Bcast(&blocklen,1,MPI_DOUBLE,0,MPI_COMM_WORLD);//den eimai sigouros gia ton communicator
-	
-	// Create local blocks for the processors
-   
-
-	MPI_Bcast(&blocklen,1,MPI_INT,0,proc_grid);
-
-	if(my_id!=root)
-	{
-		printf("Proc.%d: length received is %d\n",my_id,blocklen );
 	}
+
+
+	printf("Welcome. I am process No %d / coords: %d.%d\n",my_id,coords[0],coords[1]);
+
+
+	limit = sqrt(nproc);
+	count = n/limit;
+	blocklength = n/limit;
+	stride = n;
+
+	// Divide the blocks
+	MPI_Type_vector(count,blocklength,stride,MPI_DOUBLE,&newtype);
+	MPI_Type_commit(&newtype);
+
+	// Create space for each process's block
+	blockA=(double*)calloc(count*count*sizeof(double));
+	blockB=(double*)calloc(count*count*sizeof(double));
+	blockC=(double*)calloc(count*count*sizeof(double));
+
+	// Root process sends each other process their assigned block of data
+	if(my_id==root) {
+		for(row=0;row<row_size;row++){
+			for(col=0;col<column_size;col++){
+				pos[0]=row;
+				pos[1]=col;
+				MPI_Cart_rank(proc_grid,pos,&temp_rank);
+				x=(row*n+col)*count;
+				MPI_Isend(&A[x],1,newtype,temp_rank,111,proc_grid,&r1);
+				MPI_Isend(&B[x],1,newtype,temp_rank,222,proc_grid,&r2);
+			}
+		}
+	}
+
+	// The other processes receive their assigned block of data
+	if(my_id!=root) {
+		MPI_Recv(blockA,count*count,MPI_DOUBLE,0,111,proc_grid, &status);
+		MPI_Recv(blockB,count*count,MPI_DOUBLE,0,222,proc_grid, &status);
+	}
+
+	// Measure the processing time
+	if(my_id==root){
+		start_time = clock();
+	}
+
 
 	// Part to show the result C matrix
 	if(my_id==root) {
